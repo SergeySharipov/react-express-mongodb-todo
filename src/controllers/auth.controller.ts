6d6c694caaa3ea3975c23d2d24effdbdb1bc375d
SergeySharipov <sharipov14@gmail.com>
2022-02-13 12:17:50 -0500

    Change valid token duration to 1 year

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -92,7 +92,7 @@ const signin = (req: Request, res: Response) => {
             }
 
             var token = Jwt.sign({ id: user.id }, config.secret, {
-                expiresIn: 86400 // 24 hours
+                expiresIn: 31556952 // 1 year
             });
 
             var authorities = [];
